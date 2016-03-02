ab97821d8e576cfd23a41c8049856eea594f7a06
Bruno Oliveira <bruno@abstractj.org>
2016-03-02 11:52:26 -0300

    Rename the C library name to make it compatible with the RPM package to be released

--- federation/freeipa/keycloak-dbus-java/src/main/java/cx/ath/matthew/unix/UnixServerSocket.java
+++ federation/freeipa/keycloak-dbus-java/src/main/java/cx/ath/matthew/unix/UnixServerSocket.java
@@ -33,7 +33,7 @@ import java.io.IOException;
  */
 public class UnixServerSocket
 {
-   static { System.loadLibrary("unix-java"); }
+   static { System.loadLibrary("unix_dbus_java"); }
    private native int native_bind(String address, boolean abs) throws IOException;
    private native void native_close(int sock) throws IOException;
    private native int native_accept(int sock) throws IOException;
