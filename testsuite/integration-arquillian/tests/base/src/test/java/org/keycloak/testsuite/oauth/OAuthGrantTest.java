e1c0b1eec6228c0bfa103040d2e249e3803f9734
Bruno Oliveira <bruno@abstractj.org>
2016-05-09 10:52:49 -0300

    All tests passing

--- testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/oauth/OAuthGrantTest.java
+++ testsuite/integration-arquillian/tests/base/src/test/java/org/keycloak/testsuite/oauth/OAuthGrantTest.java
@@ -23,23 +23,18 @@ import org.junit.Test;
 import org.keycloak.OAuth2Constants;
 import org.keycloak.admin.client.resource.ClientResource;
 import org.keycloak.admin.client.resource.RealmResource;
-import org.keycloak.admin.client.resource.RoleMappingResource;
+import org.keycloak.admin.client.resource.UserResource;
 import org.keycloak.common.constants.KerberosConstants;
 import org.keycloak.events.Details;
 import org.keycloak.events.EventType;
-import org.keycloak.models.AdminRoles;
-import org.keycloak.models.ImpersonationConstants;
 import org.keycloak.protocol.oidc.OIDCLoginProtocol;
 import org.keycloak.representations.AccessToken;
-import org.keycloak.representations.idm.ClientRepresentation;
 import org.keycloak.representations.idm.EventRepresentation;
 import org.keycloak.representations.idm.ProtocolMapperRepresentation;
 import org.keycloak.representations.idm.RealmRepresentation;
 import org.keycloak.representations.idm.RoleRepresentation;
-import org.keycloak.representations.idm.UserRepresentation;
 import org.keycloak.testsuite.AbstractKeycloakTest;
 import org.keycloak.testsuite.AssertEvents;
-import org.keycloak.testsuite.admin.ApiUtil;
 import org.keycloak.testsuite.pages.AccountApplicationsPage;
 import org.keycloak.testsuite.pages.AppPage;
 import org.keycloak.testsuite.pages.OAuthGrantPage;
@@ -47,18 +42,16 @@ import org.keycloak.testsuite.util.ClientManager;
 import org.keycloak.testsuite.util.OAuthClient;
 import org.keycloak.testsuite.util.ProtocolMapperUtil;
 import org.keycloak.testsuite.util.RoleBuilder;
-import org.keycloak.testsuite.util.UserManager;
 import org.openqa.selenium.By;
 
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.keycloak.testsuite.admin.AbstractAdminTest.loadJson;
 import static org.keycloak.testsuite.admin.ApiUtil.findClientByClientId;
+import static org.keycloak.testsuite.admin.ApiUtil.findUserByUsernameId;
 
 /**
  * @author <a href="mailto:vrockai@redhat.com">Viliam Rockai</a>
@@ -232,8 +225,8 @@ public class OAuthGrantTest extends AbstractKeycloakTest {
                 .addProtocolMapper(protocolMapper)
                 .addScopeMapping(newRole);
 
-        UserRepresentation userResource = ApiUtil.findUserByUsername(appRealm, "test-user@localhost");
-        appRealm.users().get(userResource.getId()).roles().realmLevel().add(Collections.singletonList(newRole));
+        UserResource userResource = findUserByUsernameId(appRealm, "test-user@localhost");
+        userResource.roles().realmLevel().add(Collections.singletonList(newRole));
 
         // Confirm grant page
         grantPage.assertCurrent();
@@ -296,41 +289,10 @@ public class OAuthGrantTest extends AbstractKeycloakTest {
         RoleRepresentation fooRole = appRealm.roles().get("foo-role").toRepresentation();
         ClientManager.realm(appRealm).clientId(THIRD_PARTY_APP).addScopeMapping(fooRole);
 
-        UserRepresentation testUser = ApiUtil.findUserByUsername(appRealm, "test-user@localhost");
-        /*appRealm.users().get(testUser.getId()).roles().realmLevel().add(Arrays.asList(fooRole, barAppRole));
-        appRealm.users().get(testUser.getId()).roles().clientLevel(THIRD_PARTY_APP).add(Collections.singletonList(barAppRole));*/
+        UserResource testUser = findUserByUsernameId(appRealm, "test-user@localhost");
 
-        ClientResource testRealmClient = ApiUtil.findClientResourceByClientId(appRealm, THIRD_PARTY_APP);
-        List<RoleRepresentation> roles = new LinkedList<>();
-        roles.add(ApiUtil.findClientRoleByName(testRealmClient, "bar-role").toRepresentation());
-
-        appRealm.users().get(testUser.getId()).roles().clientLevel(testRealmClient.toRepresentation().getId()).add(roles);
-        appRealm.users().get(testUser.getId()).roles().realmLevel().add(Collections.singletonList(fooRole));
-
-        appRealm.users().get(testUser.getId()).roles().getAll().getClientMappings();
-        appRealm.users().get(testUser.getId()).roles().getAll().getRealmMappings();
-
-
-
-
-        /*keycloakRule.update(new KeycloakRule.KeycloakSetup() {
-
-            @Override
-            public void config(RealmManager manager, RealmModel adminstrationRealm, RealmModel appRealm) {
-                ClientModel thirdParty = appRealm.getClientByClientId("third-party");
-                RoleModel barAppRole = thirdParty.addRole("bar-role");
-                barAppRole.setScopeParamRequired(true);
-
-                RoleModel fooRole = appRealm.addRole("foo-role");
-                fooRole.setScopeParamRequired(true);
-                thirdParty.addScopeMapping(fooRole);
-
-                UserModel testUser = manager.getSession().users().getUserByUsername("test-user@localhost", appRealm);
-                testUser.grantRole(fooRole);
-                testUser.grantRole(barAppRole);
-            }
-
-        });*/
+        testUser.roles().clientLevel(thirdParty.toRepresentation().getId()).add(Collections.singletonList(barAppRole));
+        testUser.roles().realmLevel().add(Collections.singletonList(fooRole));
 
         // Assert roles not on grant screen when not requested
 
@@ -365,16 +327,8 @@ public class OAuthGrantTest extends AbstractKeycloakTest {
         events.expect(EventType.REVOKE_GRANT)
                 .client("account").detail(Details.REVOKED_CLIENT, THIRD_PARTY_APP).assertEvent();
         // cleanup
-        /*keycloakRule.update(new KeycloakRule.KeycloakSetup() {
-
-            @Override
-            public void config(RealmManager manager, RealmModel adminstrationRealm, RealmModel appRealm) {
-                appRealm.removeRole(appRealm.getRole("foo-role"));
-                ClientModel thirdparty = appRealm.getClientByClientId("third-party");
-                thirdparty.removeRole(thirdparty.getRole("bar-role"));
-            }
-
-        });*/
+        appRealm.roles().deleteRole(fooRole.getName());
+        thirdParty.roles().deleteRole(barAppRole.getName());
 
     }
 
