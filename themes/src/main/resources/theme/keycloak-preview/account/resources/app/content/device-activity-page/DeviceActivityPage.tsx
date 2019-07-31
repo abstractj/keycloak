904fd618824a5254b59aadc9f4cbf7c1509220ff
Bruno Oliveira da Silva <bruno@abstractj.org>
2019-07-31 07:45:38 -0300

    WIP

--- themes/src/main/resources/theme/keycloak-preview/account/resources/app/content/device-activity-page/DeviceActivityPage.tsx
+++ themes/src/main/resources/theme/keycloak-preview/account/resources/app/content/device-activity-page/DeviceActivityPage.tsx
@@ -15,21 +15,160 @@
  */
 
 import * as React from 'react';
+import {
+  Title,
+  Button,
+  DataList,
+  DataListItem,
+  DataListItemRow,
+  DataListCell,
+  DataListCheck,
+  DataListAction,
+  DataListToggle,
+  DataListContent,
+  DataListItemCells
+} from '@patternfly/react-core';
+import {ContentPage} from '../ContentPage';
  
 export interface DeviceActivityPageProps {
 }
  
 export class DeviceActivityPage extends React.Component<DeviceActivityPageProps> {
     
-    public constructor(props: DeviceActivityPageProps) {
-        super(props);
-    }
+  public constructor(props: DeviceActivityPageProps) {
+    super(props);
+    this.state = {
+      expanded: [],
+      isOpen1: false,
+      isOpen2: false,
+      isOpen3: false
+    };
 
-    public render(): React.ReactNode {
-        return (
+    this.onToggle1 = isOpen1 => {
+      this.setState({ isOpen1 });
+    };
+
+    this.onToggle2 = isOpen2 => {
+      this.setState({ isOpen2 });
+    };
+
+  }
+
+  public render(): React.ReactNode {
+    const toggle = id => {
+      const expanded = this.state.expanded;
+      const index = expanded.indexOf(id);
+      const newExpanded =
+        index >= 0 ? [...expanded.slice(0, index), ...expanded.slice(index + 1, expanded.length)] : [...expanded, id];
+      this.setState(() => ({ expanded: newExpanded }));
+    };
+    return (
+        <DataList aria-label="device-activity">
+          <DataListItem aria-labelledby="device-activity-item">
+            <DataListItemRow>
+              <DataListItemCells
+                dataListCells={[
+                  <DataListCell key="device-activity-cell">
+                    <Title headingLevel="h3" size="2xl">
+                      Device Activity
+                    </Title>
+                  </DataListCell>,
+                ]}
+              />
+            </DataListItemRow>
+          </DataListItem>
+          <DataListItem aria-labelledby="simple-item2">
+            <DataListItemRow>
+              <DataListToggle
+                onClick={() => toggle('ex-toggle1')}
+                isExpanded={this.state.expanded.includes('ex-toggle1')}
+                id="ex-toggle1"
+                aria-controls="ex-expand1"
+              </>
+              <DataListItemCells
+                dataListCells={[
+                  <DataListCell key="secondary content2" width="1">
+                    <div>
+                      <b id="width-ex3-item1">Firefox</b>
+                      <p>IP: 111.111.111.111</p>
+                      <p>Last Access: NaN</p>
+                    </div>
+                  </DataListCell>,
+                  <DataListCell isFilled={false} alignRight key="secondary content4">
+                    <Button>Logout</Button>
+                  </DataListCell>
+                ]}
+              />
+            </DataListItemRow>
+            <DataListContent
+              aria-label="Primary Content Details"
+              id="ex-expand1"
+              isHidden={!this.state.expanded.includes('ex-toggle1')}
+            >
+              <div>
+                <p>Client:</p>
+                <p>Started:</p>
+                <p>Expires:</p>
+              </div>
+
+            </DataListContent>
+          </DataListItem>
+
+          <DataListItem aria-labelledby="simple-item1">
+            <DataListItemRow>
+              <DataListToggle
+                onClick={() => toggle('ex-toggle2')}
+                isExpanded={this.state.expanded.includes('ex-toggle2')}
+                id="ex-toggle2"
+                aria-controls="ex-expand2"
+              </>
+            <DataListItemCells
+              dataListCells={[
+                <DataListCell key="secondary content2" width="1">
+                  <div>
+                    <b id="width-ex3-item1">Opera 22.33</b>
+                    <p>IP: 111.111.111.111</p>
+                    <p>Last Access: NaN</p>
+                  </div>
+                </DataListCell>,
+                <DataListCell isFilled={false} alignRight key="secondary content4">
+                  <Button>Logout</Button>
+                </DataListCell>
+              ]}
+            />
+          </DataListItemRow>
+          <DataListContent
+            aria-label="Primary Content Details"
+            id="ex-expand2"
+            isHidden={!this.state.expanded.includes('ex-toggle2')}
+          >
             <div>
-              <h2>Hello Device Activity Page</h2>
+              <p>Client:</p>
+              <p>Started:</p>
+              <p>Expires:</p>
             </div>
-        );
-    }
+
+          </DataListContent>
+        </DataListItem>
+          <DataListItem aria-labelledby="simple-item1">
+            <DataListItemRow>
+              <DataListItemCells
+                dataListCells={[
+                  <DataListCell key="primary content3">
+                    <a href="http://">View all history</a>
+                  </DataListCell>
+                ]}
+              />
+              <DataListAction
+                aria-labelledby="check-action-item2 check-action-action2"
+                id="check-action-action2"
+                aria-label="Actions"
+              >
+                <Button>Logout All Devices</Button>
+              </DataListAction>
+            </DataListItemRow>
+          </DataListItem>
+        </DataList>
+    );
+  }
 };
